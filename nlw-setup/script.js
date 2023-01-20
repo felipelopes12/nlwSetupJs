7c2d125da52b0b3bb6b4d1ae47aaf4de6ab376c4
Amadeu Filipe Lopes <felipe301016@hotmail.com>
2023-01-20 01:02:38 -0300

    day4

--- nlw-setup/script.js
+++ nlw-setup/script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')//(function) o usuario adicionara os eventos
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
+button.addEventListener('click', add)// adicionando o click e uma function
+form.addEventListener("change", save)// salvar pagina quando for recarregada
+
+function add() {
+const today = new Date().toLocaleDateString("pt-br").slice(0, -5) // no click puxa o dia de hoje
+const dayExists = nlwSetup.dayExists(today)
+
+if(dayExists) { // alerta para o usuario de dia incluso(boolean)
+  alert("Dia já incluso 🔴")
+  return
+} 
+
+alert('Adicionado com sucesso ✅')
+ nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+function save() { //transformando o objeto em texto
+  localStorage.setItem('NLWSetup@habitsDays', JSON.stringify(nlwSetup.data)) 
+  
+}
+
+  //transformando o texto em objeto
+const data = JSON.parse(localStorage.getItem('NLWSetup@habitsDays')) || {}
+ nlwSetup.setData(data)
+ nlwSetup.load()
